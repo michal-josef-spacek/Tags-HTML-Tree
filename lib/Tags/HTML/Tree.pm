7e33fd53a89104e9a7e2cb96441e2b96b7ff4700
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-18 09:30:45 +0200

    Prepare for 0.06 version

--- lib/Tags/HTML/Tree.pm
+++ lib/Tags/HTML/Tree.pm
@@ -12,7 +12,7 @@ use Mo::utils::CSS 0.07 qw(check_css_class check_css_unit);
 use Scalar::Util qw(blessed);
 use Unicode::UTF8 qw(decode_utf8);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -674,6 +674,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
